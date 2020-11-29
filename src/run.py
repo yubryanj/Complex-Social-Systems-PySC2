c17f4a1e270b4a53dfb8896ea4058a963143eb76
Bryan Yu <bryany@kth.se>
2020-11-29 23:50:39 +0100

    Updated cumulative reward

--- src/run.py
+++ src/run.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--experiment_id', type=str, help='id of experiment', default='000')
-    parser.add_argument('--total_episodes', type=int, help='number of episodes to run for', default='10')
+    parser.add_argument('--total_episodes', type=int, help='number of episodes to run for', default='2')
     parser.add_argument('--apply_incentive', dest='apply_incentive', help='include incentive structure in the rewards', action='store_true')
     parser.add_argument('--continue-training', dest='continue_training', help='to continue training using weights already learned', action='store_true')
     parser.add_argument('--episodic_rewards', dest='episodic_rewards', help='return rewards only at the end of an episode', action='store_true')
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     parser.add_argument('--algorithm', type=str, help='(RANDOM,PPO,A2C,DQN)', default='PPO')
     parser.add_argument('--log_dir', type=str, help='name of the log directory', default='logs/')
     parser.add_argument('--log_name', type=str, help='name of the log', default='logs')
-    parser.add_argument('--timesteps', type=int, help='number of timesteps to train for', default=1e6) 
+    parser.add_argument('--timesteps', type=int, help='number of timesteps to train for', default=1) 
 
     # Convert to a dictionary 
     parameters = vars(parser.parse_args())
@@ -132,9 +132,6 @@ if __name__ == "__main__":
         # Inititalize the reward for this episode
         episode_reward = 0.0
 
-        # Collect the cumulative episode reward
-        cumulative_reward = 0.0
-
         # Collect the cumulative reward at every step
         minerals_collected = []
                 
@@ -158,10 +155,7 @@ if __name__ == "__main__":
                 episode_reward = info[0]['minerals_collected']
 
             # Update the cumulative minerals collected
-            minerals_collected = info[0]['minerals_collected']
-
-            # Store in the step rewards
-            minerals_collected.append(cumulative_reward)
+            minerals_collected.append(info[0]['minerals_collected'])
 
         # Store the reward of the next episode
         episode_rewards.append(episode_reward)
